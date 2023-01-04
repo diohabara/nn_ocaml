83847923569b79ff701e38ec4bad381422f79577
卍diohabara卍 <diohabara@gmail.com>
2023-01-04 21:32:29 +0900

    feat: finish 010

--- lib/nn_ocaml.ml
+++ lib/nn_ocaml.ml
@@ -118,3 +118,19 @@ let%test_unit "009 Pack consecutive duplicates" =
     ; [ "e"; "e"; "e"; "e" ]
     ]
 ;;
+
+let encode list =
+  let rec aux count acc = function
+    | [] -> []
+    | [ x ] -> (count + 1, x) :: acc
+    | a :: (b :: _ as t) ->
+      if phys_equal a b then aux (count + 1) acc t else aux 0 ((count + 1, a) :: acc) t
+  in
+  List.rev (aux 0 [] list)
+;;
+
+let%test_unit "010 Run-length encoding" =
+  [%test_eq: (int * string) list]
+    (encode [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ])
+    [ 4, "a"; 1, "b"; 2, "c"; 2, "a"; 1, "d"; 4, "e" ]
+;;
